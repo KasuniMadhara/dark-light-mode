2ff5fbf5c5520c641fd8751f51067294c0076886
KMWijerathna <168425680+KMWijerathna@users.noreply.github.com>
2025-11-16 18:33:50 +0530

    updated dark-light mode

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const button = document.getElementById("toggleBtn");
+const icon = document.getElementById("icon");
 
 button.addEventListener("click", ()=>{
     document.body.classList.toggle("dark-mode");
+    document.body.classList.toggle("light-mode");
+
+    if(document.body.classList.contains("dark-mode")) {
+    icon.classList.remove("bi-moon-fill");
+    icon.classList.add("bi-sun-fill");
+}else{
+    icon.classList.remove("bi-sun-fill");
+    icon.classList.add("bi-moon-fill");
+}
 
 });
+
+
+
